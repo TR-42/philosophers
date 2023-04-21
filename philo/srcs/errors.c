28c55581ec303c0edf131d079c800fc3ea9b038c
kfujita <kfujita@student.42tokyo.jp>
2023-04-21 23:57:32 +0900

    `print_err`関数を実装

---
+++ philo/srcs/errors.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 23:40:09 by kfujita           #+#    #+#             */
+/*   Updated: 2023/04/21 23:56:53 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "errors.h"
+
+#include "libft.h"
+
+// - STDERR_FILENO
+// - write
+#include <unistd.h>
+
+int	print_err(const char *msg)
+{
+	size_t	len;
+
+	len = ft_strlen(msg);
+	if (len != 0)
+		write(STDERR_FILENO, msg, len);
+	return (1);
+}
