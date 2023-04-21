0eaca56a1ad651225a1edb7e498f623771ff2177
kfujita <kfujita@student.42tokyo.jp>
2023-04-21 23:50:38 +0900

    create `strlen` / `strnlen`

--- philo/srcs/libft/ft_strlen.c
+++ philo/srcs/libft/ft_strlen.c
@@ -1,22 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/21 23:41:53 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/21 23:50:20 by kfujita          ###   ########.fr       */
+/*   Created: 2023/04/21 23:42:59 by kfujita           #+#    #+#             */
+/*   Updated: 2023/04/21 23:49:56 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-// - size_t
-# include <stddef.h>
+#include <errno.h>
 
-size_t	ft_strlen(const char *str);
-size_t	ft_strnlen(const char *str, size_t maxlen);
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
 
-#endif
+	len = 0;
+	if (str == NULL)
+		errno = EFAULT;
+	else
+		while (*(str++) != '\0')
+			len++;
+	return (len);
+}
+
+size_t	ft_strnlen(const char *str, size_t maxlen)
+{
+	size_t	len;
+
+	len = 0;
+	if (str == NULL)
+		errno = EFAULT;
+	else
+		while (len < maxlen && *(str++) != '\0')
+			len++;
+	return (len);
+}
