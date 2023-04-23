2d887191b77943a221474e2c0d2a996473dee578
kfujita <kfujita@student.42tokyo.jp>
2023-04-23 22:11:20 +0900

    ログ出力用関数を追加

--- philo/headers/philo.h
+++ philo/headers/philo.h
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 22:29:24 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/23 13:23:43 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/23 22:07:03 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,28 @@
 
 # include <pthread.h>
 
+// - bool
+# include <stdbool.h>
+
 // - size_t
 # include <stddef.h>
+
+// - timeval
+# include <sys/time.h>
 
 typedef pthread_t		t_thrd;
 typedef pthread_mutex_t	t_mtx;
 typedef struct s_app	t_app;
+typedef struct timeval	t_timeval;
 
-enum	e_philo_state
+typedef enum e_philo_state
 {
+	take_a_fork,
 	eating,
 	thinking,
 	sleeping,
-};
+	dead,
+}	t_philo_state;
 
 typedef struct s_philo
 {
@@ -54,5 +63,7 @@ struct s_app
 	t_philo	*philos;
 	t_mtx	*forks;
 };
+
+bool	print_log(t_app *d, t_timeval time, size_t num, t_philo_state state);
 
 #endif
---
+++ philo/srcs/print_log.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_log.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/23 21:49:36 by kfujita           #+#    #+#             */
+/*   Updated: 2023/04/23 22:10:44 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// - printf
+#include <stdio.h>
+
+#include "philo.h"
+
+bool	print_log(t_app *d, t_timeval time, size_t num, t_philo_state state)
+{
+	int			ret;
+	const char	*s_state;
+
+	s_state = "UNKNOWN STATE";
+	if (state == eating)
+		s_state = "has taken a fork";
+	else if (state == eating)
+		s_state = "is eating";
+	else if (state == thinking)
+		s_state = "is thinking";
+	else if (state == sleeping)
+		s_state = "is sleeping";
+	else if (state == dead)
+		s_state = "died";
+	if (pthread_mutex_lock(&(d->print_lock)) != 0)
+		return (false);
+	ret = printf("%ld%4ld %zu %s\n",
+			time.tv_sec, time.tv_usec / 1000l, num, s_state);
+	return (pthread_mutex_unlock(&(d->print_lock)) == 0 && ret > 0);
+}
