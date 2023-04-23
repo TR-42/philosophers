2766c2d6408d81e4c8ef69ea1de09200cd887807
kfujita <kfujita@student.42tokyo.jp>
2023-04-23 19:39:55 +0900

    mutexの初期化を行うようにした

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,18 +6,26 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 22:29:39 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/23 18:51:49 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/23 19:33:11 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // - errno
 #include <errno.h>
 
+// - pthread_mutex_init
+// - PTHREAD_MUTEX_INITIALIZER
+#include <pthread.h>
+
 // - bool
 #include <stdbool.h>
 
 // - UINTPTR_MAX
 #include <stdint.h>
+
+// - malloc
+// - free
+#include <stdlib.h>
 
 #include "errors.h"
 #include "libft.h"
@@ -27,6 +35,29 @@ static bool	_set(const char *str, size_t	*variable)
 {
 	*variable = ft_atop(str, NULL);
 	return (errno == 0);
+}
+
+// f[0] p[0] f[1] p[1] ... f[LAST] p[LAST] f[0]
+static int	_t_app_init_philo_mutex(t_app *d)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < d->philo_cnt)
+	{
+		d->forks[i] = (t_mtx){0};
+		if (pthread_mutex_init(d->forks + i, NULL) != 0)
+			return (print_err_dispose_mem_mtx("mutex init failed", d, i));
+		d->philos[i].d = d;
+		d->philos[i].num = i + 1;
+		d->philos[i].fork_l = d->forks + i;
+		if (i == (d->philo_cnt - 1))
+			d->philos[i].fork_r = d->forks;
+		else
+			d->philos[i].fork_r = d->forks + i + 1;
+		i++;
+	}
+	return (0);
 }
 
 static int	_t_app_init(t_app *d, int argc, const char *argv[])
@@ -39,7 +70,13 @@ static int	_t_app_init(t_app *d, int argc, const char *argv[])
 		|| !_set(argv[3], &(d->eat_ms)) || !_set(argv[4], &(d->sleep_ms))
 		|| (argc == 6 && !_set(argv[5], &(d->eat_quota))))
 		return (print_err("Invalid argument format (Parse Error)"));
-	return (0);
+	if (d->philo_cnt <= 0 || d->die_ms <= 0 || d->philo_cnt == UINTPTR_MAX)
+		return (print_err("Invalid argument (some argument out of range)"));
+	d->philos = ft_calloc_nofill(d->philo_cnt, sizeof(t_philo));
+	d->forks = ft_calloc_nofill(d->philo_cnt, sizeof(t_mtx));
+	if (d->philos == NULL || d->forks == NULL)
+		return (print_err_dispose_mem("malloc failed", d));
+	return (_t_app_init_philo_mutex(d));
 }
 
 int	main(int argc, const char *argv[])
@@ -48,7 +85,8 @@ int	main(int argc, const char *argv[])
 	int		ret;
 
 	ret = _t_app_init(&d, argc, argv);
-	if (ret != 0)
+	if (ret != 0 || d.eat_quota == 0)
 		return (ret);
+	print_err_dispose_mem_mtx(NULL, &d, d.philo_cnt);
 	return (0);
 }
