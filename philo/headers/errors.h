a9629d3bf854284a58b7d7b9b9536d2e6da2a081
kfujita <kfujita@student.42tokyo.jp>
2023-04-23 19:27:57 +0900

    add `print_err_dispose_mem_mtx` function

--- philo/headers/errors.h
+++ philo/headers/errors.h
@@ -6,14 +6,20 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 23:40:19 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/23 19:04:26 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/23 19:31:38 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
+// - size_t
+# include <stddef.h>
+
+# include "philo.h"
+
 int	print_err(const char *msg);
 int	print_err_dispose_mem(const char *msg, t_app *d);
+int	print_err_dispose_mem_mtx(const char *msg, t_app *d, size_t mtx_cnt);
 
 #endif
--- philo/srcs/errors.c
+++ philo/srcs/errors.c
@@ -6,13 +6,16 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 23:40:09 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/23 19:03:52 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/23 19:31:51 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "errors.h"
 
 #include "libft.h"
+
+// - free
+#include <stdlib.h>
 
 // - STDERR_FILENO
 // - write
@@ -38,4 +41,15 @@ int	print_err_dispose_mem(const char *msg, t_app *d)
 	d->philos = NULL;
 	d->forks = NULL;
 	return (1);
+}
+
+int	print_err_dispose_mem_mtx(const char *msg, t_app *d, size_t mtx_cnt)
+{
+	size_t	i;
+
+	i = 0;
+	if (d->forks != NULL)
+		while (i < mtx_cnt)
+			pthread_mutex_destroy(d->forks + i++);
+	return (print_err_dispose_mem(msg, d));
 }
