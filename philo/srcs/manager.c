57841f4ce33069d671742d4ccd0b780b9510eafc
kfujita <kfujita@student.42tokyo.jp>
2023-04-28 23:48:09 +0900

    philoの食事部分を分離

--- philo/srcs/manager.c
+++ philo/srcs/manager.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 22:36:53 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/28 23:10:07 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/28 23:47:38 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,37 +51,49 @@ static t_pstat	_set_last_eat(t_philo *p, t_tv tv)
 	return (eating);
 }
 
-static bool	philo_action(t_philo *p)
+static bool	philo_eat(t_philo *p, t_tv *tv)
 {
-	t_tv	tv;
 	bool	success;
 
 	if (pthread_mutex_lock(p->fork_l) != 0)
 		return (false);
-	if (gettimeofday(&tv, NULL) || t_tv_ispassed(&tv, &(p->deadline))
-		|| print_log(p->d, tv, p->num, take_a_fork))
+	if (gettimeofday(tv, NULL) != 0)
 		return (pthread_mutex_unlock(p->fork_l) >= 0);
-	if (pthread_mutex_lock(p->fork_r) && pthread_mutex_unlock(p->fork_l) >= 0)
-		return (false);
-	success = (gettimeofday(&tv, NULL) == 0
-			&& !t_tv_ispassed(&tv, &(p->deadline))
-			&& print_log(p->d, tv, p->num, take_a_fork)
-			&& print_log(p->d, tv, p->num, _set_last_eat(p, tv))
-			&& t_tv_addms(&tv, p->d->eat_ms)
-			&& !t_tv_ispassed(&tv, &(p->deadline)) && sleeper(tv, &tv));
+	if (t_tv_ispassed(tv, &(p->deadline)))
+		return (pthread_mutex_unlock(p->fork_l) != 0);
+	if (!print_log(p->d, *tv, p->num, take_a_fork)
+		|| pthread_mutex_lock(p->fork_r))
+		return (pthread_mutex_unlock(p->fork_l) >= 0);
+	success = gettimeofday(tv, NULL);
+	if (success && t_tv_ispassed(tv, &(p->deadline)))
+		return (pthread_mutex_unlock(p->fork_l)
+			* pthread_mutex_unlock(p->fork_r) * 0);
+	success = (success && print_log(p->d, *tv, p->num, take_a_fork));
+	success = (success && print_log(p->d, *tv, p->num, _set_last_eat(p, *tv)));
+	success = (success && gettimeofday(tv, NULL) != 0);
+	if (success && !t_tv_ispassed(tv, &(p->deadline)))
+		success = sleeper(*tv, tv);
 	(void)(pthread_mutex_unlock(p->fork_l) + pthread_mutex_unlock(p->fork_r));
-	if (!success)
+	return (success);
+}
+
+static bool	philo_action(t_philo *p, t_tv *tv)
+{
+	if (!is_sim_end_or_set_state(p, sleeping)
+		|| !print_log(p->d, *tv, p->num, sleeping)
+		|| !t_tv_addms(tv, p->d->sleep_ms))
 		return (false);
-	return (is_sim_end_or_set_state(p, sleeping)
-		&& print_log(p->d, tv, p->num, sleeping)
-		&& t_tv_addms(&tv, p->d->sleep_ms)
-		&& (t_tv_ispassed(&tv, &(p->deadline))
-			|| (sleeper(tv, &tv) && print_log(p->d, tv, p->num, thinking))));
+	if (t_tv_ispassed(tv, &(p->deadline)))
+		return (true);
+	if (!sleeper(*tv, tv))
+		return (false);
+	return (print_log(p->d, *tv, p->num, thinking));
 }
 
 void	*philo_soul(void *_p)
 {
 	t_philo	*p;
+	t_tv	tv;
 	size_t	count;
 
 	p = (t_philo *)_p;
@@ -90,7 +102,7 @@ void	*philo_soul(void *_p)
 	count = 0;
 	while (p->d->is_noquota || count++ < p->d->eat_quota)
 	{
-		if (!philo_action(p))
+		if (!philo_eat(p, &tv) || !philo_action(p, &tv))
 		{
 			_state(p, err);
 			break ;
