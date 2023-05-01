435fda684a38bcab9c4c261833f93a7cf70f0973
kfujita <kfujita@student.42tokyo.jp>
2023-05-01 22:26:14 +0900

    リークチェックを追加

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,9 +6,21 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 22:29:39 by kfujita           #+#    #+#             */
-/*   Updated: 2023/05/01 00:50:25 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/01 22:18:33 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// (for debug)
+// - system
+#include <stdlib.h>
+
+// (for debug)
+// - getpid
+#include <unistd.h>
+
+// (for debug)
+// - sprintf
+#include <stdio.h>
 
 #include "errors.h"
 #include "philo.h"
@@ -29,3 +41,17 @@ int	main(int argc, const char *argv[])
 	print_err_dispose_mem_mtx(NULL, &d, d.philo_cnt);
 	return (0);
 }
+
+#if DEBUG
+
+# define DEBUG_LEAKS_CMD_LEN (32)
+
+__attribute__((destructor))
+static void	destructor(void) {
+	char	cmd[DEBUG_LEAKS_CMD_LEN];
+
+	snprintf(cmd, DEBUG_LEAKS_CMD_LEN, "leaks %d", getpid());
+	system(cmd);
+}
+
+#endif
