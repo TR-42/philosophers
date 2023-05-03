d321e6563970b41806fed4e52172da825a57bd84
kfujita <kfujita@student.42tokyo.jp>
2023-05-04 00:37:08 +0900

    計算単位をmsに変更
    ms単位では生きてても、us単位では生きてるはずなことがあったため

--- philo/srcs/sleeper.c
+++ philo/srcs/sleeper.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 00:22:57 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/30 00:23:48 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/04 00:35:37 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,26 @@ bool	sleeper(t_tv target, t_tv *now)
 
 bool	t_tv_addms(t_tv *target, size_t ms)
 {
-	if (!can_addl(target->tv_sec, (ms / 1000) + (target->tv_usec / 1000000)))
+	int	target_ms;
+	int	s_overflow;
+
+	target_ms = target->tv_usec / 1000;
+	s_overflow = (ms + target_ms) / 1000;
+	if (!can_addl(target->tv_sec, s_overflow))
 		return (false);
-	target->tv_usec = (((ms % 1000) * 1000) + target->tv_usec);
-	target->tv_sec += (ms / 1000) + (target->tv_usec / 1000000);
-	target->tv_usec %= 1000000;
+	target->tv_usec = ((ms + target_ms) % 1000) * 1000;
+	target->tv_sec += s_overflow;
 	return (true);
 }
 
 bool	t_tv_ispassed(const t_tv *now, const t_tv *target)
 {
+	int	now_ms;
+	int	target_ms;
+
+	now_ms = now->tv_usec / 1000;
+	target_ms = target->tv_usec / 1000;
 	return (now->tv_sec > target->tv_sec
 		|| (now->tv_sec == target->tv_sec
-			&& now->tv_usec >= target->tv_usec));
+			&& now_ms >= target_ms));
 }
